1feedd5df2a22e10b997c1693e55fe9084cd597d
hp <hp@LAPTOP-T4BTENLV>
2023-01-13 23:46:56 +0530

    This is my first commit

---
+++ TestMaven/src/main/java/com/example/TestMaven/App.java
@@ -0,0 +1,14 @@
+package com.example.TestMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
